e207bececc4cdc2df48dde81e1b7bc2c46338e6f
AgathePons <ponsagathe37@gmail.com>
2023-05-19 13:12:11 +0200

    add new DTO for task with logs

---
+++ src/main/java/com/api/dto/TaskWithLogDto.java
@@ -0,0 +1,18 @@
+package com.api.dto;
+
+import com.api.entities.Log;
+import com.api.entities.utilEnum.TaskStatus;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+public class TaskWithLogDto extends TaskDto {
+    private Set<Log> logs;
+}
